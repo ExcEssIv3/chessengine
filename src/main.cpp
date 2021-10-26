94aaca05008e6777437c772829d0bbeb0ae88b83
ExcEssIv3 <sethrowland@tamu.edu>
2021-10-26 18:23:57 -0500

    added queen movement functionality

--- src/main.cpp
+++ src/main.cpp
@@ -8,17 +8,13 @@ using namespace BOARD;
 using namespace PIECE;
 
 int main() {
-    board testboard = board("8/8/8/8/4p3/8/5P2/8 w - - 0 1");
-    list<vector<short>> moves = testboard.getLegalMovesAtIndex({4, 5});
+    board testboard = board("8/1r1r1r2/8/1r1q1r2/8/1r1r1r2/8/8 w - - 0 1");
+    list<vector<short>> moves = testboard.getLegalMovesAtIndex({4, 3});
 
     for (list<vector<short>>::iterator it = moves.begin(); it != moves.end(); it++) {
         vector<short> move = *it;
         cout << move[0] << " " << move[1] << endl;
     }
     // testboard.printMailbox();
-    testboard.movePiece(vector<short>({1, 5}), vector<short>({3, 5}));  
-    testboard.movePiece(vector<short>({3, 4}), vector<short>({2, 5}));
-    testboard.printMailbox();
-    cout << testboard.getEnPassant()[0] << ", " << testboard.getEnPassant()[1] << endl;
 
 }
--- src/piece/queen.cpp
+++ src/piece/queen.cpp
@@ -35,5 +35,107 @@ queen::queen(const queen& q) {
 };
 
 list<vector<short>> queen::getLegalMoves(const vector<vector<piece*>>& positions) {
-    return list<vector<short>>{};
+    list<vector<short>> legalPositions;
+    short distUp = 7 - position[0];
+    short distLeft = position[1];
+    short distDown = position[0];
+    short distRight = 7 - position[1];
+
+    short maxDistance = 0;
+    short oppositeColor = (color == 0) ? 1 : 0;
+
+    // up left
+    maxDistance = (distUp < distLeft) ? distUp : distLeft;
+    for (short i = 1; i <= maxDistance; i++) {
+        if (positions[position[0] + i][position[1] - i]->getColor() == color) {
+            break;
+        }
+        legalPositions.emplace_back(vector<short>({static_cast<short>(position[0] + i), static_cast<short>(position[1] - i)}));
+        if (positions[position[0] + i][position[1] - i]->getColor() == oppositeColor) {
+            break;
+        }
+    }
+
+    // up right
+    maxDistance = (distUp < distRight) ? distUp : distRight;
+    for (short i = 1; i <= maxDistance; i++) {
+        if (positions[position[0] + i][position[1] + i]->getColor() == color) {
+            break;
+        }
+        legalPositions.emplace_back(vector<short>({static_cast<short>(position[0] + i), static_cast<short>(position[1] + i)}));
+        if (positions[position[0] + i][position[1] + i]->getColor() == oppositeColor) {
+            break;
+        }
+    }
+
+    // down right
+    maxDistance = (distDown < distRight) ? distDown : distRight;
+    for (short i = 1; i <= maxDistance; i++) {
+        if (positions[position[0] - i][position[1] + i]->getColor() == color) {
+            break;
+        }
+        legalPositions.emplace_back(vector<short>({static_cast<short>(position[0] - i), static_cast<short>(position[1] + i)}));
+        if (positions[position[0] - i][position[1] + i]->getColor() == oppositeColor) {
+            break;
+        }
+    }
+
+    // down left
+    maxDistance = (distDown < distLeft) ? distDown : distLeft;
+    for (short i = 1; i <= maxDistance; i++) {
+        if (positions[position[0] - i][position[1] - i]->getColor() == color) {
+            break;
+        }
+        legalPositions.emplace_back(vector<short>({static_cast<short>(position[0] - i), static_cast<short>(position[1] - i)}));
+        if (positions[position[0] - i][position[1] - i]->getColor() == oppositeColor) {
+            break;
+        }
+    }
+
+
+    // up
+    for (int i = 1; i <= distUp; i++) {
+        if (positions[position[0] + i][position[1]]->getColor() == color) {
+            break;
+        }
+        legalPositions.emplace_back(vector<short>({static_cast<short>(position[0] + i), position[1]}));
+        if (positions[position[0] + i][position[1]]->getColor() == oppositeColor) {
+            break;
+        }
+    }
+
+    // down
+    for (int i = 1; i <= distDown; i++) {
+        if (positions[position[0] - i][position[1]]->getColor() == color) {
+            break;
+        }
+        legalPositions.emplace_back(vector<short>({static_cast<short>(position[0] - i), position[1]}));
+        if (positions[position[0] - i][position[1]]->getColor() == oppositeColor) {
+            break;
+        }
+    }
+
+    // left
+    for (int i = 1; i <= distLeft; i++) {
+        if (positions[position[0]][position[1] - i]->getColor() == color) {
+            break;
+        }
+        legalPositions.emplace_back(vector<short>({position[0], static_cast<short>(position[1] - i)}));
+        if (positions[position[0]][position[1] - i]->getColor() == oppositeColor) {
+            break;
+        }
+    }
+
+    // right
+    for (int i = 1; i <= distRight; i++) {
+        if (positions[position[0]][position[1] + i]->getColor() == color) {
+            break;
+        }
+        legalPositions.emplace_back(vector<short>({position[0], static_cast<short>(position[1] + i)}));
+        if (positions[position[0]][position[1] + i]->getColor() == oppositeColor) {
+            break;
+        }
+    }
+
+    return legalPositions;
 };
